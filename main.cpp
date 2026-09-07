0a73c828998bbd2004c61b5bedf76d5609c4ddcb
lnunes-ui <luisguilhermejnunesdasilva45@gmail.com>
2026-09-06 20:59:47 -0400

    Adicionado loop de tentativas e dicas de maior ou menor

--- main.cpp
+++ main.cpp
@@ -3,10 +3,25 @@ using namespace std;
 int main() {
     int numsecret = 73;
     int x;
-    cout << "Advinhe o número secreto (entre 1 e 100): " << "\n";
-    cin >> x;
-    if(x == numsecret){cout << "Acertou!" << endl;}
-    else {cout << "Errou!";}
+    bool acertou = false;
+    int cont = 0;
+    while(acertou == false) {
+        cout << "Advinhe o número secreto (entre 1 e 100): " << "\n";
+        cin >> x;
+        cont++;
+        if(x == numsecret){
+            acertou = true;
+            break;
+        } else if (x > numsecret) {
+            cout << "O número é MENOR!";
+        } else {cout << "O número é MAIOR!";}
+    
+    }
+    if(acertou == true){
+        cout << "ACERTOU! O Número é "<< numsecret << "\n";
+        cout << "Vocé demorou " << cont << " tentativas!" << "\n";
+    }
+   
 
 
 
