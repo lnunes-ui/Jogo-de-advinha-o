dc067cc75dd980ebc025469da614351a34a166ab
lnunes-ui <luisguilhermejnunesdasilva45@gmail.com>
2026-09-06 20:58:43 -0400

    Estrutura inicial com numero secreto fixo e uma tentativa

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int numsecret = 73;
+    int x;
+    cout << "Advinhe o número secreto (entre 1 e 100): " << "\n";
+    cin >> x;
+    if(x == numsecret){cout << "Acertou!" << endl;}
+    else {cout << "Errou!";}
+
+
+
+
+
+
+
+
+
+    return 0;
+}
